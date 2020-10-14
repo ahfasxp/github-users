fca15c62a40e96926fea0c745301a68646acc9e4
ahfasxp <ahfas.fazria@gmail.com>
2020-10-14 15:09:49 +0700

    add splashscreen

--- app/src/main/java/com/ahfasxp/githubusers/SplashScreenActivity.kt
+++ app/src/main/java/com/ahfasxp/githubusers/SplashScreenActivity.kt
@@ -1,6 +1,9 @@
 package com.ahfasxp.githubusers
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -9,5 +12,9 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
