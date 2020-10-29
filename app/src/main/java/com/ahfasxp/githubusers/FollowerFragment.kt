d8446bcdd7ccad0aaa66a60a3787d0dd633bf9a8
ahfasxp <ahfas.fazria@gmail.com>
2020-10-29 17:35:49 +0700

    "Fix TabLayout Follower dan Following"

--- app/src/main/java/com/ahfasxp/githubusers/FollowerFragment.kt
+++ app/src/main/java/com/ahfasxp/githubusers/FollowerFragment.kt
@@ -7,7 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
 import kotlinx.android.synthetic.main.fragment_follower.*
+import kotlinx.android.synthetic.main.fragment_following.*
+import org.json.JSONArray
+import org.json.JSONObject
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,12 +27,12 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class FollowerFragment : Fragment() {
-    lateinit var tvTest: TextView
+    private lateinit var adapter: SearchUserAdapter
 
     companion object {
         private val ARG_USERNAME = "username"
 
-        fun newInstance(username: String): HomeFragment{
+        fun newInstance(username: String?): FollowerFragment {
             val fragment = FollowerFragment()
             val bundle = Bundle()
             bundle.putString(ARG_USERNAME, username)
@@ -44,13 +51,65 @@ class FollowerFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        tvTest = view.findViewById(R.id.tv_username)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-            val userName = arguments?.getString(ARG_USERNAME)
-            Log.d("cekusername", userName.toString())
-            tv_username.text = userName
+        val username = arguments?.getString(ARG_USERNAME)
+        Log.d("cekusername", username.toString())
+        if (username != null) {
+            setUser(username)
+        }
+        //recycleview dari api
+        adapter = SearchUserAdapter()
+        adapter.notifyDataSetChanged()
+        rv_list_follower.layoutManager = LinearLayoutManager(activity)
+        rv_list_follower.adapter = adapter
+    }
+
+    fun setUser(username: String) {
+        val listItems = ArrayList<SearchUser>()
+        val url = "https://api.github.com/users/$username/followers"
+        val client = AsyncHttpClient()
+        client.addHeader("Authorization", "ad81bb7c78a5023aa15e58403d1cb5b8ac813d32")
+        client.addHeader("User-Agent", "request")
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<Header>,
+                responseBody: ByteArray
+            ) {
+                try {
+                    //parsing json
+                    val result = String(responseBody)
+                    val responsArray = JSONArray(result)
+                    val list = responsArray
+
+                    for (i in 0 until list.length()) {
+                        val dataUser = list.getJSONObject(i)
+                        val user = SearchUser()
+                        user.name = dataUser.getString("login")
+                        user.username = dataUser.getString("login")
+                        user.avatar = dataUser.getString("avatar_url")
+                        listItems.add(user)
+                    }
+
+                    //set data ke adapter
+                    adapter.setData(listItems)
+                } catch (e: Exception) {
+                    Log.d("Exception", e.message.toString())
+                }
+
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<Header>,
+                responseBody: ByteArray,
+                error: Throwable
+            ) {
+                Log.d("onFailure", error.message.toString())
+            }
+        })
     }
 }
--- app/src/main/java/com/ahfasxp/githubusers/FollowingFragment.kt
+++ app/src/main/java/com/ahfasxp/githubusers/FollowingFragment.kt
@@ -1,10 +1,21 @@
 package com.ahfasxp.githubusers
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_follower.*
+import kotlinx.android.synthetic.main.fragment_following.*
+import org.json.JSONArray
+import org.json.JSONObject
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,15 +28,17 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class FollowingFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var adapter: SearchUserAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    companion object {
+        private val ARG_USERNAME = "username"
+
+        fun newInstance(username: String?): FollowingFragment {
+            val fragment = FollowingFragment()
+            val bundle = Bundle()
+            bundle.putString(ARG_USERNAME, username)
+            fragment.arguments = bundle
+            return fragment
         }
     }
 
@@ -37,23 +50,67 @@ class FollowingFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_following, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FollowingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FollowingFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val username = arguments?.getString(ARG_USERNAME)
+        Log.d("cekusername", username.toString())
+        if (username != null) {
+            setUser(username)
+        }
+        //recycleview dari api
+        adapter = SearchUserAdapter()
+        adapter.notifyDataSetChanged()
+        rv_list_following.layoutManager = LinearLayoutManager(activity)
+        rv_list_following.adapter = adapter
+    }
+
+    fun setUser(username: String) {
+        val listItems = ArrayList<SearchUser>()
+        val url = "https://api.github.com/users/$username/following"
+        val client = AsyncHttpClient()
+        client.addHeader("Authorization", "ad81bb7c78a5023aa15e58403d1cb5b8ac813d32")
+        client.addHeader("User-Agent", "request")
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<Header>,
+                responseBody: ByteArray
+            ) {
+                try {
+                    //parsing json
+                    val result = String(responseBody)
+                    val responsArray = JSONArray(result)
+                    val list = responsArray
+
+                    for (i in 0 until list.length()) {
+                        val dataUser = list.getJSONObject(i)
+                        val user = SearchUser()
+                        user.name = dataUser.getString("login")
+                        user.username = dataUser.getString("login")
+                        user.avatar = dataUser.getString("avatar_url")
+                        listItems.add(user)
+                    }
+
+                    //set data ke adapter
+                    adapter.setData(listItems)
+                } catch (e: Exception) {
+                    Log.d("Exception", e.message.toString())
                 }
+
             }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<Header>,
+                responseBody: ByteArray,
+                error: Throwable
+            ) {
+                Log.d("onFailure", error.message.toString())
+            }
+        })
     }
 }
